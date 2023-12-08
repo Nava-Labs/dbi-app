7f3b310326765488880daeb584622e3328e09dae
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-12-09 00:53:16 +0530

    feat: created footer and implemented on app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main className="h-full flex flex-col justify-between items-center bg-neutral-900">
           <Header />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full px-20 py-3 border-t-[1px] border-neutral-600">
+      <div className="flex justify-between">
+        <div className="w-full flex flex-col justify-center items-center">
+          <h1 className="text-sm mb-2 text-neutral-600 font-medium">
+            Created by:
+          </h1>
+          <Link href="https://navalabs.io/" target="_blank">
+            <img
+              src="/nava-labs-logo.png"
+              alt="Nava Labs Logo"
+              className="h-3"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
