ad12a4668d448e5dfce33c86e31138eb56f54e40
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-12-09 01:05:42 +0530

    feat: created connect wallet button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { WalletIcon } from "@heroicons/react/24/outline";
+import { ConnectKitButton } from "connectkit";
 import Link from "next/link";
 import React from "react";
 
@@ -9,7 +13,20 @@ export default function Header() {
           <h1 className="font-semibold text-lg">Decentralized</h1>
           <h1 className="font-semibold text-lg">Bureau of Investigation</h1>
         </Link>
-        <div>{/* TODO: Connect button */}</div>
+        <div>
+          <ConnectKitButton.Custom>
+            {({ isConnected, show, truncatedAddress, chain }) => {
+              return (
+                <div className="flex w-full justify-center items-center text-base font-medium px-5 py-2 border rounded-full border-neutral-600 hover:border-neutral-300 align-middle cursor-pointer">
+                  <button onClick={show}>
+                    {isConnected ? truncatedAddress : "Connect wallet"}
+                  </button>
+                  <WalletIcon className="h-5 ml-2" />
+                </div>
+              );
+            }}
+          </ConnectKitButton.Custom>
+        </div>
       </div>
     </div>
   );
