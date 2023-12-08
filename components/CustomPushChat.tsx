e69e3d7db78734de130372c5642a6a173861097e
jonassunandar <jonassunandar@yahoo.com>
2023-12-09 01:45:51 +0530

    feat: send and fetch message

--- components/CustomPushChat.tsx
+++ components/CustomPushChat.tsx
@@ -29,11 +29,16 @@ export function useEthersSigner ({ chainId }: { chainId?: number } = {}) {
    [walletClient]
  )
 }
+function formatDID (did: string) {
+ const splitted = did.split(':')
+ return splitted[1]
+}
 
 export default function CustomPushChat(props: any) {
  const { groupChatId } = props
 
  const [user, setUser] = useState<PushAPI>()
+ const [message, setMessage] = useState('')
 
  const [groupChats, setGroupChats] = useState<any>()
  const { data: walletClient, isError, isLoading } = useWalletClient()
@@ -54,8 +59,63 @@ export default function CustomPushChat(props: any) {
    setGroupChats(groupChats)
  }
  }, [walletClient])
+ 
+ const handleJoinGroup = async () => {
+  if (user) {
+    try {
+      const joinGroup = await user.chat.group.join(groupChatId)
+      console.log("console join group ", joinGroup);
+    } catch (err) {
+      console.log('error join group ', err)
+    }
+  }
+}
+const handleSendMessage = async () => {
+ if (message.trim() !== '') {
+   if (user) {
+     try {
+       console.log('message ', message, user)
+       await user.chat.send(groupChatId, {
+         type: 'Text',
+         content: message
+       })
+     } catch (err) {
+       console.log(err)
+     }
+   } else {
+     console.log('error handle send message : user not found')
+   }
+   setMessage('')
+ }
+}
+const handleInputChange = (e: any) => {
+ setMessage(e.target.value)
+}
 
  return (
-  <div></div>
+  <div>
+   <button onClick={handleJoinGroup}>Send</button>
+      <br></br>
+      <input
+        type='text'
+        value={message}
+        onChange={handleInputChange}
+        placeholder='Type your message...'
+      />
+      <button onClick={handleSendMessage}>Send</button>
+    <ol>
+        <li>yoo</li>
+        {groupChats &&
+          groupChats?.map(function (chat: any) {
+            return (
+              <li>
+                <p>From: {formatDID(chat.fromDID)}</p>
+                <p>Chat: {chat.messageContent}</p>
+                <p>Timestamp: {chat.timestamp}</p>
+              </li>
+            )
+          })}
+      </ol>
+  </div>
  )
 }
