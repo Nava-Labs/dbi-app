6bc3d61fd7b98f78f0ffa46288c263bbebd90af3
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-12-09 12:04:11 +0530

    feat: added route to org portal

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import CreateOrganizationDialog from "@/components/CreateOrganizationDialog";
 import SearchForm from "@/components/SearchForm";
 import { getWebsiteContent } from "@/shared/utils/storage/entityActions";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
   let [websiteContent, setWebsiteContent] = useState<any>();
@@ -31,25 +32,24 @@ export default function Home() {
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {websiteContent?.organisations.map((item: any, index: any) => (
-          <div
-            key={index}
-            className="flex flex-col items-center justify-center h-64 w-[190px] transition-all p-2 border-[1px] rounded-xl border-neutral-600 hover:border-neutral-400 cursor-pointer text-base shadow-md shadow-neutral-700"
-          >
-            <div className="w-full p-4 leading-5 sm:leading-6">
-              <div className="flex items-center justify-center mb-2">
-                <img
-                  src={item.logoUrl}
-                  alt="Organization Logo"
-                  className="rounded-full bg-neutral-900 object-cover h-20 w-20 mb-1"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center truncate">
-                <div className="mb-0 mt-0 h-[32px] overflow-hidden pb-0 text-lg font-medium">
-                  {item.name}
+          <Link key={index} href={item.name}>
+            <div className="flex flex-col items-center justify-center h-64 w-[190px] transition-all p-2 border-[1px] rounded-xl border-neutral-600 hover:border-neutral-400 cursor-pointer text-base shadow-md shadow-neutral-700">
+              <div className="w-full p-4 leading-5 sm:leading-6">
+                <div className="flex items-center justify-center mb-2">
+                  <img
+                    src={item.logoUrl}
+                    alt="Organization Logo"
+                    className="rounded-full bg-neutral-900 object-cover h-20 w-20 mb-1"
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-center truncate">
+                  <div className="mb-0 mt-0 h-[32px] overflow-hidden pb-0 text-lg font-medium">
+                    {item.name}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
